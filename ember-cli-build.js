c0d013aadc1619081db54a7a00a43206b2051961
Dan Lubbers <lubbers.dan@gmail.com>
2019-05-26 22:48:53 -0400

    added css

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -20,5 +20,12 @@ module.exports = function(defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
 
+// ember-cli-build.js
+var app = new EmberApp(defaults, {
+  sassOptions: {
+    extension: 'scss'
+  }
+});
+
   return app.toTree();
 };
